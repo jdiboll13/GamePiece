a439da66073c3b54c777cb4aa0cf5f1856113bd3
Jeremiah Diboll <jdiboll@gmail.com>
2017-08-15 13:18:25 -0400

    The piece moves, freezes, unfreezes.

--- Program.cs
+++ Program.cs
@@ -12,15 +12,53 @@ namespace GamePiece
             public string Color { get; set; }
             public string Name { get; set; }
 
-            public int Move(int posx, int posy)
+            public int MoveRight()
             {
                 if (Frozen == false)
                 {
                     PositionX++;
-                    PositionY++;
+                    return PositionX;
                 }
-
-            
+                else
+                {
+                    return PositionX;
+                }
+            }
+            public int MoveLeft()
+            {
+                if (Frozen == false)
+                {
+                    PositionX--;
+                    return PositionX;
+                }
+                else
+                {
+                    return PositionX;
+                }
+            }
+            public int MoveUp()
+            {
+                if (Frozen == false)
+                {
+                    PositionY++;
+                    return PositionY;
+                }
+                else
+                {
+                    return PositionY;
+                }
+            }
+            public int MoveDown()
+            {
+                if (Frozen == false)
+                {
+                    PositionY--;
+                    return PositionY;
+                }
+                else
+                {
+                    return PositionY;
+                }
             }
             public Boolean Freeze()
             {
@@ -41,11 +79,23 @@ namespace GamePiece
             piece1.Color = "Red";
             piece1.Name = "Bertha";
 
+            piece1.MoveRight();
+            Console.WriteLine($"{piece1.Color} {piece1.Name} is at X:{piece1.PositionX} and Y:{piece1.PositionY}");
+
             piece1.Freeze();
             Console.WriteLine($"{piece1.Color} {piece1.Name} is frozen.");
 
+            piece1.MoveUp();
+            Console.WriteLine($"{piece1.Color} {piece1.Name} is at X:{piece1.PositionX} and Y:{piece1.PositionY}");
+
             piece1.Unfreeze();
-            Console.WriteLine($"{piece1.Color} {piece1.Name} is frozen.");
+            Console.WriteLine($"{piece1.Color} {piece1.Name} is unfrozen.");
+
+            piece1.MoveLeft();
+            Console.WriteLine($"{piece1.Color} {piece1.Name} is at X:{piece1.PositionX} and Y:{piece1.PositionY}");
+
+            piece1.MoveDown();
+            Console.WriteLine($"{piece1.Color} {piece1.Name} is at X:{piece1.PositionX} and Y:{piece1.PositionY}");
 
         }
     }
